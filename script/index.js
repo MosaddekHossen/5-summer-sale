eb7c601d8e8bf3ba3a79ee3c827909285c362bb5
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-08-21 00:17:57 +0600

    next commit

--- script/index.js
+++ script/index.js
@@ -1,21 +1,13 @@
 let count = 0;
-function handlerClickBtn(target){
+let total = 0;
+function handlerClickBtn(target) {
     const selectedItemContainer = document.getElementById('selected-item');
     const itemName = target.childNodes[5].innerText;
-    count ++;
+    count++;
     const li = document.createElement('li');
     li.innerText = `${count}. ${itemName}`;
     selectedItemContainer.appendChild(li);
+    const price = target.childNodes[7].innerText.split(' ')[0];
+    total = parseFloat(total) + parseFloat(price);
+    const firstTotal = document.getElementById('total').innerText = total;
 }
-
-// let total = 0;
-// function handleCLikBtn(target) {
-//   const selectedItemContainer = document.getElementById("selected-items");
-//   const itemName = target.parentNode.childNodes[1].innerText;
-//   const li = document.createElement("li");
-//   li.innerText = itemName;
-//   selectedItemContainer.appendChild(li);
-//   const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
-//   total = parseInt(total) + parseInt(price);
-//   document.getElementById("total").innerText = total;
-// }
