039da9a8cb62c4fa01b0651eb1c858262e7a5c05
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-08-20 22:20:02 +0600

    next commit

--- script/index.js
+++ script/index.js
@@ -1,44 +1,14 @@
 let count = 0;
-function prod1(element){
+function handlerClickBtn(target){
     const selectedItemContainer = document.getElementById('selected-item');
-    const itemName = document.getElementById('Accessories').innerText;
+    const itemName = target.childNodes[5].innerText;
     count ++;
     const li = document.createElement('li');
     li.innerText = `${count}. ${itemName}`;
-    // li.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;  
-    selectedItemContainer.appendChild(li);    
+    selectedItemContainer.appendChild(li);
 }
 
-// let count = 0; // Initialize the count variable
-// function prod1(element) {
-//     const selectedItemContainer = document.getElementById('selected-item');
-//     const itemName = document.getElementById('Accessories').innerText;
-    
-//     count++; // Increment the count variable
-    
-//     const li = document.createElement('li');
-//     li.innerText = `${count}. ${itemName}`;
-//     // You can add more content or modify the formatting of the list item here
-
-//     selectedItemContainer.appendChild(li);
-// }
-
-// function addToCalculationEntry(areaType, area) {
-//     console.log(areaType + " " + area)
-//     const calculationEntry = document.getElementById('calculation-entry');
-
-//     const count = calculationEntry.childElementCount;
-
-//     const p = document.createElement('p');
-//     p.classList.add('my-4');
-//     // p.innerHTML = areaType + " " + area;
-//     p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
-
-//     calculationEntry.appendChild(p);
-// }
-
 // let total = 0;
-
 // function handleCLikBtn(target) {
 //   const selectedItemContainer = document.getElementById("selected-items");
 //   const itemName = target.parentNode.childNodes[1].innerText;
