28855ce2d15c02882cca009231fa39245716ab5f
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-08-21 16:46:31 +0600

    next commit

--- script/index.js
+++ script/index.js
@@ -11,27 +11,41 @@ function handlerClickBtn(target) {
     total = parseFloat(total) + parseFloat(price);
     const totalFinal = total.toFixed(2);
     const firstTotal = document.getElementById('total').innerText = totalFinal;
+    const firstTotalNumber = parseFloat(firstTotal);
+    if (firstTotalNumber > 0) {
+        document.querySelector('.purchaseBtn').disabled = false;
+    }
+    else {
+        document.querySelector('purchaseBtn').disabled = true;
+    }
+    if (firstTotalNumber >= 200) {
+        document.getElementById('applyId').disabled = false;
+    }
+    else {
+        document.getElementById('applyId').disabled = true;
+    }
     // Discount
-    const discount = firstTotal / 100 * 20; 
-    const discountUpdate = document.getElementById('discountId');
-    const discountFinal = discount.toFixed(2);
-    discountUpdate.innerText = discountFinal;
+    let discount = 0;
+    if (total >= 200) {
+        const couponInput = document.getElementById("couponInput");
+        const applyCouponBtn = document.getElementById("applyId");
+        applyCouponBtn.addEventListener("click", () => {
+            const couponCode = couponInput.value;
+            if (couponCode === 'SELL200') {
+                const discountUpdate = document.getElementById('discountId');
+                const discountFinal = discount.toFixed(2);
+                discountUpdate.innerText = discountFinal;
+                discount = total / 100 * 20;
+                const totalPri = firstTotal - discount;
+                const totalFiled = document.getElementById('final-price');
+                const totalPriceFinal = totalPri.toFixed(2);
+                totalFiled.innerText = totalPriceFinal;
+            }
+        });
+    }
     // Total
     const totalPri = firstTotal - discount;
     const totalFiled = document.getElementById('final-price');
     const totalPriceFinal = totalPri.toFixed(2);
     totalFiled.innerText = totalPriceFinal;
-}
-
-// function resetValue(){
-//     totalFinal.value = " ";
-// }
-// console.log(resetValue)
-
-// function resetValue() {
-//     document.body.style.body = 'red';
-// }
-// const applyBtn = document.getElementById('applyId');
-// applyBtn.addEventListener('click',function(){
-
-// })
+} 
