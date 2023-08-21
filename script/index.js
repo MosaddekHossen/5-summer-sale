4c97aeab0a2e7b63965927f9e332037bfb803930
MosaddekHossen <mosaddekhossen7496@gmail.com>
2023-08-21 11:15:20 +0600

    next commit

--- script/index.js
+++ script/index.js
@@ -9,5 +9,21 @@ function handlerClickBtn(target) {
     selectedItemContainer.appendChild(li);
     const price = target.childNodes[7].innerText.split(' ')[0];
     total = parseFloat(total) + parseFloat(price);
-    const firstTotal = document.getElementById('total').innerText = total;
+    const totalFinal = total.toFixed(2);
+    const firstTotal = document.getElementById('total').innerText = totalFinal;
+    // Discount
+    const discount = firstTotal / 100 * 20; 
+    const discountUpdate = document.getElementById('discountId');
+    const discountFinal = discount.toFixed(2);
+    discountUpdate.innerText = discountFinal;
+    // Total
+    const totalPri = firstTotal - discount;
+    const totalFiled = document.getElementById('final-price');
+    const totalPriceFinal = totalPri.toFixed(2);
+    totalFiled.innerText = totalPriceFinal;
 }
+
+// const applyBtn = document.getElementById('applyId');
+// applyBtn.addEventListener('click',function(){
+
+// })
